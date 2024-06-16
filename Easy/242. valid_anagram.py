9f09633c84a8360200260575d077b901baec0dea
izzat-karimov <izzatcodes@gmail.com>
2024-06-16 13:45:26 +0200

    adding a solution

--- Easy/242. valid_anagram.py
+++ Easy/242. valid_anagram.py
@@ -1,20 +1,27 @@
 # Solution 1 - Hashmap #
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) != len(t): #if the lengths of s and t are different, then return False since they would not be anagrams if they had different lenths 
             return False
         
-        hashmapS, hashmapT = {}, {}
+        hashmapS, hashmapT = {}, {} #Initializing two hashmap
 
-        for i in range(len(s)):
-            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
-            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+        for i in range(len(s)): #Iterating through the length of the string s
+            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0) #Every time we see a character, we want to increment the count of that character by one. But, that character may not exist in the hashmap yet, which would result in a key error. To get around that, we use get(). Second parameter of get() is a default value. In this case, the default value in get() function is 0. This basically means that if the key does not exist in the hashmap, then the default value is 0. 
+            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0) #Doing the same thing for String t
 
-        for c in hashmapS:
-            if hashmapS[c] != hashmapT.get(c, 0):
+        for c in hashmapS: #Iterating over the each character to make sure that the counts are the same
+            if hashmapS[c] != hashmapT.get(c, 0): # If the counts are different, return False
                 return False
-        return True
+        return True #if the method has not returned False at this point, it means that the frequencies match between s and t, and that returns True.
 
-# Solution 2#
+# Solution 2 - Using the Counter() Method in Python #
 
-# Solution 3#
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t) # Counter is a data structure in Python that counts automatically. While this is just as efficient as the first solution, it would not be ideal in an interview.
+
+# Solution 3 - Sorting#
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t) 
